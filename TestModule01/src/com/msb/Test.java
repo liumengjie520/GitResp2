7c8ca7876faf268afada92a117c69a2f44bc7689
liumj <376401127@qq.com>
2021-12-13 17:52:44 +0800

    test类添加12

--- TestModule01/src/com/msb/Test.java
+++ TestModule01/src/com/msb/Test.java
@@ -1,18 +1,11 @@
 package com.msb;
 
-import java.util.Arrays;
-
 /**
- * @description: 测试
+ * @description: 测试版
  * @author: liumj
- * @create: 2021-12-13 10:52
+ * @create: 2021-12-13 17:51
  **/
 
 public class Test {
-    public static void main(String[] args) {
-    System.out.println("fff ");
-    System.out.println("333 ");
-//        Arrays.toString();
-      
-    }
+    int a=1;
 }
